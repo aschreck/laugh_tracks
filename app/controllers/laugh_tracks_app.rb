85ce8ba6c3df777ec588fadf83ccb2b0b16464d1
Aaron Schreck <schreck.aaron@gmail.com>
2017-10-06 10:56:15 -0600

    Add successful age search functionality

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -2,9 +2,17 @@ class LaughTracksApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
   
   get "/comedians" do
-    @comedians = Comedian.all
-    @specials = Special.all
+    if params[:age]
+      @comedians = Comedian.where(age: params[:age])
+    else 
+      @comedians = Comedian.all
+    end 
+  @specials = Special.all
     erb :dashboard
   end 
 
+  # get "/comedians/:age" do
+  #   @comedians = Comedian.where(params[:age])
+  #   erb :'age-dashboard'
+  # end 
 end
